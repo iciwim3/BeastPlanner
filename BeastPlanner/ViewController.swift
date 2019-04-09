f2ba34a18d06c69072d86b2a7e14821a305246c0
SKE <appybuildmore@gmail.com>
2019-04-09 03:30:14 -0700

    Initial Commit

---
+++ BeastPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeastPlanner
+//
+//  Created by Sain-R Edwards on 4/9/19.
+//  Copyright © 2019 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
